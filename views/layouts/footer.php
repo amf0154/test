555e0265b4bb42c9d379adb07acb5623dad8f875
amf0154 <gleb@sgsrv.ru>
2017-09-09 23:56:40 +0300

    for template

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -1,2 +1,8 @@
-
+<div class="panel panel-primary">
+  <div class="panel-body">
+    <center>Developer gleb@sgsrv.ru ; Special for ItSupportMee</center>
+  </div>
+</div>
+  </body>
+</html>
 
--- views/layouts/header.php
+++ views/layouts/header.php
@@ -1,2 +1,31 @@
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+<title>News Management System</title>
+<link rel="stylesheet" href="/template/css/bootstrap.min.css">
+<link rel="stylesheet" href="/template/css/bootstrap-theme.min.css">
+<script src="/template/js/bootstrap.min.js"></script>
+  </head>
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
 
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="/">News Management System</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><a class="navbar-brand" href="/add"><p style="color:#FF0000"><b>Add news</b></p></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  <br></br>
+<br></br>
 
