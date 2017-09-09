2dcc388c7dbc010c9ee18240725c34523bd1e3da
amf0154 <gleb@sgsrv.ru>
2017-09-09 23:58:16 +0300

    corrected footer

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -1,6 +1,6 @@
 <div class="panel panel-primary">
   <div class="panel-body">
-    <center>Developer gleb@sgsrv.ru ; Special for ItSupportMee</center>
+    <center>Developer gleb@sgsrv.ru ; Special for ItSupportMe</center>
   </div>
 </div>
   </body>
